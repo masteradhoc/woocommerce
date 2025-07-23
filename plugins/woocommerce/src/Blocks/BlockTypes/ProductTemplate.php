606dcca64d72f768f15207d8d5470504152abbd7
MaggieCabrera <maggie.cabrera@automattic.com>
2025-07-23 19:43:34 +0200

    add block gap manually on flow layouts

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductTemplate.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductTemplate.php
@@ -3,6 +3,7 @@
 namespace Automattic\WooCommerce\Blocks\BlockTypes;
 
 use Automattic\WooCommerce\Blocks\BlockTypes\ProductCollection\Utils as ProductCollectionUtils;
+use Automattic\WooCommerce\Blocks\Utils\StyleAttributesUtils;
 use WP_Block;
 
 /**
@@ -78,7 +79,19 @@ class ProductTemplate extends AbstractBlock {
 
 		$classnames .= ' wc-block-product-template';
 
-		$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => trim( $classnames ) ) );
+		//Apply blockGap styles manually for flow layout
+		if ( isset( $block->context['displayLayout']['type'] ) && 'flow' ) {
+			$block_gap_styles = StyleAttributesUtils::get_classes_and_styles_by_attributes( $attributes, [ 'block_gap' ] );
+					
+			$extra_attributes = array( 'class' => trim( $classnames ) );
+
+			// Apply blockGap styles if they exist
+			if ( ! empty( $block_gap_styles['styles'] ) ) {
+				$extra_attributes['style'] = $block_gap_styles['styles'];
+			}
+
+			$wrapper_attributes = get_block_wrapper_attributes( $extra_attributes );
+		}
 
 		$content = '';
 		while ( $query->have_posts() ) {
